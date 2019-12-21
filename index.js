8d66d4b75a03f8cf9c18b8e065ed82c33f000df1
Fredrik Mäkilä <fredrik.makila@gmail.com>
2019-12-21 02:11:26 +0000

    Make it more readable.

--- index.js
+++ index.js
@@ -1,63 +1,31 @@
 var intToRoman = function(num) {
     let result = ''
 
-    while (num) {
-      if (num >= 1000) {
-        result += digits('M', Math.floor(num / 1000))
-        num = num % 1000
-      } else if (num >= 500) {
-        if (num < 900) {
-          result += digits('D', Math.floor(num / 500))
-          num = num % 500
+    const values = [
+      { value: 1000, roman: 'M' },
+      { value: 500, roman: 'D', sub: { value: 900, roman: 'CM' }},
+      { value: 100, roman: 'C', sub: { value: 400, roman: 'CD' }},
+      { value: 50, roman: 'L', sub: { value: 90, roman: 'XC' }},
+      { value: 10, roman: 'X', sub: { value: 40, roman: 'XL' }},
+      { value: 5, roman: 'V', sub: { value: 9, roman: 'IX' }},
+      { value: 1, roman: 'I', sub: { value: 4, roman: 'IV' }}
+    ]
+
+    values.forEach(({ value, roman, sub }) => {
+      if (num >= value) {
+        if (!sub || num < sub.value) {
+          result += digits(roman, Math.floor(num / value))
+          num = num % value
         } else {
-          result += 'CM'
-          num = num % 100
+          result += sub.roman
+          num -= sub.value
         }
-      } else if (num >= 100) {
-        if (num < 400) {
-          result += digits('C', Math.floor(num / 100))
-          num = num % 100
-        } else {
-          result += 'CD'
-          num = num % 100
-        }
-      } else if (num >= 50) {
-        if (num < 90) {
-          result += digits('L', Math.floor(num / 50))
-          num = num % 50
-        } else {
-          result += 'XC'
-          num = num % 10
-        }
-      } else if (num >= 10) {
-        if (num < 40) {
-          result += digits('X', Math.floor(num / 10))
-          num = num % 10
-        } else {
-          result += 'XL'
-          num = num % 10
-        }
-      } else if (num >= 5) {
-        if (num < 9) {
-          result += 'V'
-          num = num % 5
-        } else {
-          result += 'IX'
-          num = 0
-        }
-      } else {
-        if (num < 4) {
-          result += digits('I', num)
-        } else {
-          result += 'IV'
-        }
-        num = 0
       }
-    }
+    })
 
     return result
 };
 
 const digits = (char, n) => new Array(n).fill(char).join('')
 
-intToRoman(58)
+intToRoman(1994)
